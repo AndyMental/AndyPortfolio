77641ae9469c4c8b6034193337648ee775b40d8f
AndyMental <andy.is.mental@gmail.com>
2023-01-02 16:08:07 +0530

    created blog scaffold

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'ffaker'
+
+Blog.destroy_all
+
+50.times do |index|
+    t = FFaker::Book.title + ' ' + FFaker::CheesyLingo.title
+    str = FFaker::HTMLIpsum.body
+    s_str = str.split("/h1>")
+    i = rand(10...100)
+    s_str.insert(1,'/h1><img src="https://picsum.photos/id/' + i.to_s + '/600/300" alt="' + t + '">')
+    p = s_str.join("")
+    Blog.create!(title: t, body: p)
+end
+
+p "Created #{Blog.count} blogs"
