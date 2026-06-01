e9f49f716be1281b30e73c3ab6a8014343b9e205
AndyMental <12481769+AndyMental@users.noreply.github.com>
2026-06-01 07:03:52 +0000

    Add static about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "up#show"
   root "blogs#index"
+  get "about" => "static_pages#about"
   resources :blogs
   get "/sitemap.xml", to: "sitemap#index", defaults: { format: :xml }, as: :sitemap
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
