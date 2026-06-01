c626fe802186582869f357536a8ded844e801a83
AndyMental <12481769+AndyMental@users.noreply.github.com>
2026-06-01 00:37:49 +0000

    Fix AND-6041: Add explicit ordering to blog index pagination
    Co-authored-by: multica-agent <github@multica.ai>

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @pagy, @blogs = pagy(Blog.all)
+    @pagy, @blogs = pagy(Blog.order(created_at: :desc))
   end
 
   # GET /blogs/1 or /blogs/1.json
--- test/integration/blogs_index_test.rb
+++ test/integration/blogs_index_test.rb
@@ -14,8 +14,9 @@ class BlogsIndexTest < ActionDispatch::IntegrationTest
 
   test "blogs index pagination" do
     # Create enough blogs to trigger pagination (default is 20 per page)
+    # We use explicit created_at to ensure order for the test
     21.times do |i|
-      Blog.create!(title: "Blog Post #{i}", body: "Body for blog post #{i}")
+      Blog.create!(title: "Blog Post #{i}", body: "Body for blog post #{i}", created_at: i.hours.ago)
     end
 
     get blogs_path
@@ -24,6 +25,8 @@ class BlogsIndexTest < ActionDispatch::IntegrationTest
     # Check for the first page posts
     assert_select "div#blogs" do
       assert_select "div[id^='blog_']", count: 20
+      # The first blog (newest) should be "Blog Post 0" because it was created 0 hours ago
+      assert_select "div[id^='blog_']", text: /Blog Post 0/, count: 1
     end
 
     # Check for pagination nav
@@ -34,6 +37,8 @@ class BlogsIndexTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "div#blogs" do
       assert_select "div[id^='blog_']", count: 1
+      # The last blog (oldest) should be "Blog Post 20"
+      assert_select "div[id^='blog_']", text: /Blog Post 20/, count: 1
     end
   end
 end
